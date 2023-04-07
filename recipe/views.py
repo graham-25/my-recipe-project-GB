238e8e0978fdcda6f7618b54e0a607d3b69f2595
graham-25 <grbills29@googlemail.com>
2023-04-07 10:42:55 +0000

    Created form with if statement, started CRUD on database

--- recipe/views.py
+++ recipe/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import item
 
 # Create your views here.
@@ -13,4 +13,10 @@ def show_recipe_list(request):
     return render(request, 'recipe/recipe_list.html', context)
 
 def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        item.objects.create(name=name, done=done)
+
+        return redirect('recipe_list')
     return render(request, 'recipe/add_item.html')
